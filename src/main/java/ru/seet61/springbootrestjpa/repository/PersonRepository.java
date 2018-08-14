60eda3e82b2b9b57cb784a596689f2410756c199
seet61 <seet_seet@mail.ru>
2018-08-14 15:48:50 +0300

    Initial commit

---
+++ src/main/java/ru/seet61/springbootrestjpa/repository/PersonRepository.java
@@ -0,0 +1,13 @@
+package ru.seet61.springbootrestjpa.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import ru.seet61.springbootrestjpa.models.Person;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long>{
+    List<Person> findByLastName(@Param("name") String name);
+}
